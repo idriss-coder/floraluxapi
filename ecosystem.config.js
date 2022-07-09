9cf6c508ded0d4989273529e9b1613791f57f395
IDRISSHACKER <idrisscoder@gmail.com>
2022-07-09 17:58:37 +0100

    Configure pm2 to autodeploement

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'src/server.js',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      user: 'aysfishh117',
+      host: '78.46.11.32',
+      ref  : 'origin/master',
+      repo: 'git@github.com:IDRISSHACKER/floraluxapi.git',
+      path: '/var/www/api.guihon.cm ',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
