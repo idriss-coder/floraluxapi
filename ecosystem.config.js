b98ae64c1e7c5ad560ad6d4a314dd2b21fcafc0a
IDRISSHACKER <idrisscoder@gmail.com>
2022-07-09 18:43:19 +0100

    Modify absolute path to locale path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,8 +10,8 @@ module.exports = {
       host: '78.46.11.32',
       ref  : 'origin/master',
       repo: 'https://github.com/IDRISSHACKER/floraluxapi.git',
-      path: '/var/www/api.guihon.cm',
-      'pre-deploy-local': '',
+      path: '/home/aysfishh117/web/api.guihon.cm',
+      'pre-deploy-local': 'npm install',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
