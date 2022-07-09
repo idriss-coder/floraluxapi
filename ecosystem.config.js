11d8fc7e6b5c92ff91b06fe4d80188f2c73230c9
IDRISSHACKER <idrisscoder@gmail.com>
2022-07-09 18:29:11 +0100

    Correct the err

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
       user: 'aysfishh117',
       host: '78.46.11.32',
       ref  : 'origin/master',
-      repo: 'git@github.com:IDRISSHACKER/floraluxapi.git',
-      path: '/var/www/api.guihon.cm ',
+      repo: 'https://github.com/IDRISSHACKER/floraluxapi.git',
+      path: '/var/www/api.guihon.cm',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
